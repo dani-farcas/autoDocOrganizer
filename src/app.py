bbcae08de4edb7a09806bc3282d289c1fa736c52
dani-farcas <100249555+dani-farcas@users.noreply.github.com>
2025-09-12 10:17:59 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -199,4 +199,4 @@ def explain_file():
 # 🚀 Start der App
 # ==========================================================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8000)
